d8145dcf2ec3b5a779fe58658fc4f029903607e7
ripka <ababagalamaga20@gmail.com>
2015-07-13 23:58:57 +0300

    'patterns'

--- js/main.js
+++ js/main.js
@@ -24,3 +24,49 @@ MYAPP.namespace = function(namespace) {
 
 var a = MYAPP.namespace("MYAPP.bla");
 var b = MYAPP.namespace("MYAPP.bla.a");
+
+var Universe = function() {
+
+	if (!(this instanceof Universe)) {
+		return new Universe();
+	}
+
+
+	var instance = this;
+
+	Universe = function() {
+		return instance;
+	}
+
+	Universe.prototype = this;
+
+	var instance = new Universe();
+
+	instance.name = 'Jack';
+
+	return instance;
+
+}
+
+
+var CarMaker = function() {
+}
+
+CarMaker.prototype.drive = function() {
+	console.log('this is car of type '+this.type);
+}
+
+CarMaker.Sedan = function() {
+	this.type = 'sedan';
+}
+
+CarMaker.Sedan.prototype = new CarMaker();
+CarMaker.Sedan.prototype.constructor = CarMaker.Sedan;
+
+CarMaker.factory = function(constr) {
+	if (typeof CarMaker[constr]!==undefined) {
+		return new CarMaker[constr]();
+	}
+}
+
+var toyota = CarMaker.factory('Sedan');
